7670da911b596be9d6d2784293a8ac0f4c2b07c7
ANUBHAV SRIVASTAVA <115628357+Anubhavsrivastava369@users.noreply.github.com>
2025-08-04 00:38:07 +0530

    Add some basic grammatically changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,9 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Anubhav</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a Full Stack web developer, <br className='sm:block hidden' />
-            user interfaces and web applications
+            Full Stack Web Developer,
+                     <br className='sm:block hidden' />  
+               focused on UI design and interactive web experiences.
           </p>
         </div>
       </div>
